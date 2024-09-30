11c18705fd85ce2256cfe25f75ade75300e79b2e
KESHAV4820 <keshav48kumar@gmail.com>
2024-09-30 19:45:44 +0530

    initial commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+//Note: to use live server extension for development, you need to change the folder name in the setting of workspace which starts like this: " Live Server › Settings: Multi Root Workspace Name This the entry point of server when you're in multiroot workspace ". there you need to change the name of the file from 'allexamstable project' to 'd3js charting' folder name. 
 
 
 if(module.hot){
