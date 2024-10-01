ed6b24b68266c22ba7f5c7fc6e7816d9ef4f858b
KESHAV4820 <keshav48kumar@gmail.com>
2024-10-01 15:12:30 +0530

    after successful data retrival and adding @parcelTransformer library and adding .parcelrc file in the root directory and changes made into the .csv file.

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,7 +3,7 @@
 //Note: to use live server extension for development, you need to change the folder name in the setting of workspace which starts like this: " Live Server › Settings: Multi Root Workspace Name This the entry point of server when you're in multiroot workspace ". there you need to change the name of the file from 'allexamstable project' to 'd3js charting' folder name. 
 
 
-import {csv} from 'd3';
+import {csv, csvParse} from 'd3';
 
 
 
@@ -14,16 +14,18 @@ if(module.hot){
 
 // const csvDataPath='./../../data/sampletestingdata.csv';// SuperVIE this way of passing the address is important as it is less error prone and helps bundler to find the file. Absolute address are not allowed for security reason. 
 
-const csvDataPath = [
-    '.',  // This refers to the current directory
-    '..', // This refers to the parent directory
-    '..', // This goes up one more level
-    'data', // Then it goes into the 'data' folder
-    'sampletestingdata.csv' // Finally, the CSV file
-].join('/');// Remember ItJust Beautiful this is how we make a address more clean for maintainance.
+// const csvDataPath = [
+//     '.',  // This refers to the current directory
+//     '..', // This refers to the parent directory
+//     '..', // This goes up one more level
+//     'data', // Then it goes into the 'data' folder
+//     'sampletestingdata.csv' // Finally, the CSV file
+// ].join('/');// Remember ItJust Beautiful this is how we make a address more clean for maintainance.
+
+import csvDataPath from './../../data/sampletestingdata.csv'; // Let Parcel handle asset
 
 
-console.log(csvDataPath);
+console.log(csvDataPath);// Code Testing.
 
     csv(csvDataPath).then(data =>{console.log(data);
     });
